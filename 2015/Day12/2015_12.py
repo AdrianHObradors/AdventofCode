3008e393abf751b27da437841cf80252ae2a75ea
AdrianHObradors <adrianhobradors@gmail.com>
2022-05-21 21:43:54 +0200

    Day 12 part 2

--- 2015/Day12/2015_12.py
+++ 2015/Day12/2015_12.py
@@ -1,4 +1,5 @@
 import json
+
 with open("input.txt") as file:
     data = file.read()
 
@@ -8,16 +9,29 @@ data = json.loads(data)
 def getsum(values):
     result = 0
     if isinstance(values, dict):
-        values = values.values()
+        # Remove first part for part 1
+        if red(values):
+            values = [0]
+        else:
+            values = values.values()  # Keep this still for part 1
     for i in values:
         if isinstance(i, int):
             result += i
         elif isinstance(i, str):
             continue
-
         else:
             result += getsum(i)
     return result
+
+
+# Part 2
+def red(values):
+    values = values.values()
+    for i in values:
+        if isinstance(i, str):
+            if i == "red":
+                return True
+    return False
 
 
 print(getsum(data))
